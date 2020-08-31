125464f34c610067cf2da63d11fa5bba0ca46c25
Arazorg <jarazorg@gmail.com>
2020-08-31 14:01:23 +0700

    isida skill

--- Assets/Scripts/Char/CharSkills.cs
+++ Assets/Scripts/Char/CharSkills.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 public class CharSkills : MonoBehaviour
 {
 #pragma warning disable 0649
@@ -6,20 +7,19 @@ public class CharSkills : MonoBehaviour
     [SerializeField] private GameObject isidaHealingLaser;
 #pragma warning restore 0649
 
-    //Isida
-    private readonly float healingLaserTime = 0.75f;
-    private float timeToHealingLaser = float.MaxValue;
-    //Isida
+    private string currentCharacter;
+    private float timeToSkill = float.MaxValue;
+    private bool isSkill;
 
     public void ChooseSkill(string character)
     {
-        Debug.Log(character);
+        currentCharacter = character;
         switch (character)
         {
             case "Legionnaire":
                 break;
             case "Isida":
-               // Isida();
+                IsidaSkillStart();
                 break;
             case "Archer":
                 break;
@@ -31,32 +31,92 @@ public class CharSkills : MonoBehaviour
                 break;
         }
     }
-    private void Isida()
+
+    void Update()
+    {
+        switch (currentCharacter)
+        {
+            case "Legionnaire":
+                break;
+            case "Isida":
+                IsidaSkillUsing();
+                break;
+            case "Archer":
+                break;
+            case "Keeper":
+                break;
+            case "Mechanic":
+                break;
+            case "Raider":
+                break;
+        }
+    }
+
+    #region IsidaSkill
+    private Dictionary<GameObject, GameObject> alliesLasers = new Dictionary<GameObject, GameObject>();
+    private void IsidaSkillStart()
     {
         var allies = GameObject.FindGameObjectsWithTag("Ally");
-        Debug.Log(allies.Length);
         foreach (var ally in allies)
         {
-            Vector3 direction = (ally.transform.position - transform.position);
-            LayerMask layerMask
-                = ~(1 << LayerMask.NameToLayer("Player") |
-                        1 << LayerMask.NameToLayer("Ignore Raycast") |
-                            1 << LayerMask.NameToLayer("Room"));
-            RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, layerMask);
-
             var laser = Instantiate(isidaHealingLaser, transform.position, Quaternion.identity);
+            alliesLasers.Add(ally, laser);
+        }
+        timeToSkill = Time.time + 2f;
+        isSkill = true;
+        IsidaSkill(alliesLasers);
+    }
+
+    private void IsidaSkillUsing()
+    {
+        if (Time.time < timeToSkill && isSkill)
+            IsidaSkill(alliesLasers);
+        else
+        {
+            foreach (var item in alliesLasers)
+                Destroy(item.Value);
+            alliesLasers.Clear();
+            isSkill = false;
+        }
+    }
+
+    private void IsidaSkill(Dictionary<GameObject, GameObject> alliesLasers)
+    {
+        foreach (var allyLaser in alliesLasers)
+        {
+            var ally = allyLaser.Key;
+            var laser = allyLaser.Value;
             var laserScale = new Vector3(0.8f, (ally.transform.position - transform.position).magnitude);
             laser.GetComponent<SpriteRenderer>().size = laserScale;
-           
             laser.transform.position
             = new Vector3((ally.transform.position.x + transform.position.x) / 2,
                             (ally.transform.position.y + transform.position.y) / 2);
-            //Debug.Log(transform.position - , ally.transform.position));
-            if (transform.position.x == 1)
-                laser.transform.rotation = Quaternion.Euler(new Vector3(0, 0, -Mathf.Atan2(ally.transform.position.x - transform.position.x,
-                                            laser.transform.position.y - laser.transform.position.y)
-                                                * Mathf.Rad2Deg));
-            Destroy(laser, 2f);
+            Vector2 diference = transform.position - ally.transform.position;
+            float sign = (transform.position.y < ally.transform.position.y) ? -1.0f : 1.0f;
+            float angle = (transform.position.y < ally.transform.position.y) ? -90f : 90f;
+            angle += Vector2.Angle(Vector2.right, diference) * sign;
+            laser.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
         }
     }
+    #endregion IsidaSkill
+
+    #region LegionnaireSkill
+
+    #endregion  LegionnaireSkill
+
+    #region ArcherSkill
+
+    #endregion ArcherSkill
+
+    #region MechanicSkill
+
+    #endregion MechanickSkill
+
+    #region KeeperSkill
+
+    #endregion KeeperSkill
+
+    #region RaiderSkill
+
+    #endregion RaiderSkill
 }
